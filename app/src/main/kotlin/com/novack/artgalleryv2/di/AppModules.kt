b8e96a46a760b3e15f5243de9c1c11839666a7c8
novack-anggelo <anggelo.novack@gmail.com>
2026-08-31 14:49:37 +0200

    Implemented and tested NetworkModule added buildConfigs in build.gradle

--- app/src/main/kotlin/com/novack/artgalleryv2/di/AppModules.kt
+++ app/src/main/kotlin/com/novack/artgalleryv2/di/AppModules.kt
@@ -1,5 +1,8 @@
 package com.novack.artgalleryv2.di
 
+import com.novack.artgalleryv2.core.network.di.networkModule
 import org.koin.core.module.Module
 
-val appModules: List<Module> = emptyList()
+val appModules: List<Module> = listOf(
+    networkModule,
+)
