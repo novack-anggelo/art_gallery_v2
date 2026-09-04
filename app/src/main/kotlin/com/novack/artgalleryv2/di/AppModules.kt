444b08947517d1a3422db733b21547fbe37f52a8
novack-anggelo <anggelo.novack@gmail.com>
2026-09-04 16:41:11 +0200

    ArtworkRepository and NetworkArtworkRepository implementation. Tested in NetworkArtworkRepositoryTest. updated AppModules and added DataModule

--- app/src/main/kotlin/com/novack/artgalleryv2/di/AppModules.kt
+++ app/src/main/kotlin/com/novack/artgalleryv2/di/AppModules.kt
@@ -1,8 +1,10 @@
 package com.novack.artgalleryv2.di
 
+import com.novack.artgalleryv2.core.data.di.dataModule
 import com.novack.artgalleryv2.core.data.remote.di.networkModule
 import org.koin.core.module.Module
 
 val appModules: List<Module> = listOf(
     networkModule,
+    dataModule,
 )
