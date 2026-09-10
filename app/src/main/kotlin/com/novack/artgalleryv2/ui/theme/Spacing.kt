113e0c3ff6e3c0675a0f075fe8c0c632e9fb9c22
novack-anggelo <anggelo.novack@gmail.com>
2026-09-10 12:51:47 +0200

    Added preview Images and error image. Created Spacing

---
+++ app/src/main/kotlin/com/novack/artgalleryv2/ui/theme/Spacing.kt
@@ -0,0 +1,17 @@
+package com.novack.artgalleryv2.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Spacing {
+    val SizeXXXS = 2.dp
+    val SizeXXS = 4.dp
+    val SizeXS = 8.dp
+    val SizeS = 12.dp
+    val SizeM = 16.dp
+    val SizeL = 24.dp
+    val SizeXL = 32.dp
+    val SizeXXL = 48.dp
+    val SizeXXXL = 64.dp
+    val SizeXXXXL = 128.dp
+    val SizeXXXXXL = 256.dp
+}
